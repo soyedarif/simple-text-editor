bf8ede22cf391650ba5b1692d1c3fdb9e72b9ba6
soyedarif <soyedariff@gmail.com>
2023-02-14 17:34:48 +0600

    toggle button fix-1

--- js/script.js
+++ js/script.js
@@ -19,21 +19,35 @@ document.getElementById('btn-left').addEventListener('click',function(){
     const textField=document.getElementById('text-field');
     textField.style.textAlign='left';
     this.classList.toggle('active');
+    document.getElementById('btn-center').classList.remove('active');
+    document.getElementById('btn-align').classList.remove('active');
+    document.getElementById('btn-right').classList.remove('active');
 })
 document.getElementById('btn-center').addEventListener('click',function(){
     const textField=document.getElementById('text-field');
     textField.style.textAlign='center';
     this.classList.toggle('active');
+    document.getElementById('btn-left').classList.remove('active');
+    document.getElementById('btn-right').classList.remove('active');
+    document.getElementById('btn-align').classList.remove('active');
+
 })
 document.getElementById('btn-right').addEventListener('click',function(){
     const textField=document.getElementById('text-field');
     textField.style.textAlign='right';
     this.classList.toggle('active');
+    document.getElementById('btn-center').classList.remove('active');
+    document.getElementById('btn-left').classList.remove('active');
+    document.getElementById('btn-align').classList.remove('active');
+
 })
 document.getElementById('btn-align').addEventListener('click',function(){
     const textField=document.getElementById('text-field');
     textField.style.overflowWrap='breakWord';
     this.classList.toggle('active');
+    document.getElementById('btn-center').classList.remove('active');
+    document.getElementById('btn-left').classList.remove('active');
+    document.getElementById('btn-right').classList.remove('active');
 })
 
 
